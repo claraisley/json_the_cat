6b3aacacab4fae3d96809de8cd948a9fd7908cfc
claraisley <isleyc97@gmail.com>
2019-12-05 20:12:38 +0000

    Added test file and refactored function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,19 @@
 const request = require('request');
 
-let breed = process.argv[2];
-let idName = breed.toLowerCase().slice(0, 4);
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${idName}`
-
-
-
-const breedFetcher = function(url, breed) {
+const fetchBreedDescription = function(breedName, callback) {
+  let idName = breedName.toLowerCase().slice(0, 4);
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${idName}`
 
   request(url, (error, response, body) => {
 
     const data = JSON.parse(body);
-      if (error) throw errror;
+      if (error) return callback(error);
       if (data.length === 0) {
-        console.log('That breed name does not exist')
+        callback('That breed name does not exist')
         return;
       }
-      console.log(data[0].description);
-
-
-    
+      callback(null, data[0].description);
   })
-}
+};
 
-breedFetcher(url, breed)
+module.exports =  { fetchBreedDescription };
